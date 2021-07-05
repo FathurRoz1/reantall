85c618822d7309b7546f05c881a068852aa269e8
fannisasuci <fannisasuci3@gmail.com>
2021-07-05 21:36:56 +0700

    Update web.php
    Diisi sembarang jere udin

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/customer', function () {
     return view('customer');
